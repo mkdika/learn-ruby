d47bdac72a6762a3e4e2f1f8bb1b5eea52f64b67
Maikel Chandika <mkdika@gmail.com>
2019-02-17 10:02:23 +0700

    Refine some class

--- src/rails/chapter3/config/routes.rb
+++ src/rails/chapter3/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # manual route example
+  # route / to book controller, home method
+  # get '/', to; 'book#home'
+
   resources :books
 end
